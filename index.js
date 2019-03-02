530c0c3d983437e7448176f1eff73b4cc6967a2d
GeonLee <leegun2003@gmail.com>
2019-03-03 02:14:42 +0900

    feature: new user, user number

--- index.js
+++ index.js
@@ -4,17 +4,35 @@ var io = require("socket.io")(http);
 
 const PORT = 3001;
 
+let userNum = 0;
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
 io.on("connection", function(socket) {
-  console.log("a user connected");
-  socket.on("post_message", data => {
+  userNum++;
+  // io.emit("get user number", { userNum });
+  console.log("a user connected", userNum);
+  socket.on("new user", data => {
+    console.log(data);
+    io.emit("get user number", { userNum });
+
+    io.emit("get new message", {
+      name: data,
+      message: "님이 모닥불 주위로 다가왔습니다."
+    });
+  });
+  socket.on("post message", data => {
     console.log(data);
     const { name, message, id } = data;
-    socket.broadcast.emit("get_new_message", { name, message });
+    socket.broadcast.emit("get new message", { name: name + ": ", message });
     // socket.emit("get_new_message", "can you hear me?", 1, 2, "abc");
-    io.to(`${id}`).emit("get_new_message", { name: "you", message });
+    io.to(`${id}`).emit("get new message", { name: "당신: ", message });
+  });
+  socket.on("disconnect", () => {
+    userNum--;
+    io.emit("get user number", { userNum });
   });
 });
 
