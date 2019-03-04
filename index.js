4265f249bf3bc735d8d7476722ab742e38d70d8b
GeonLee <leegun2003@gmail.com>
2019-03-04 20:18:00 +0900

    feature: disconnect event, your event

--- index.js
+++ index.js
@@ -18,23 +18,39 @@ io.on("connection", function(socket) {
   console.log("a user connected", userNum);
   socket.on("new user", data => {
     console.log(data);
+    socket.username = data;
+
     io.emit("get user number", { userNum });
 
     io.emit("get new message", {
-      name: data,
+      type: "noti",
+      name: socket.username,
       message: "님이 모닥불 주위로 다가왔습니다."
     });
   });
   socket.on("post message", data => {
     console.log(data);
-    const { name, message, id } = data;
-    socket.broadcast.emit("get new message", { name: name + ": ", message });
+    const { message, id } = data;
+    socket.broadcast.emit("get new message", {
+      type: "chat",
+      name: socket.username + ": ",
+      message
+    });
     // socket.emit("get_new_message", "can you hear me?", 1, 2, "abc");
-    io.to(`${id}`).emit("get new message", { name: "당신: ", message });
+    io.to(`${id}`).emit("get new message", {
+      type: "yours",
+      name: "당신: ",
+      message
+    });
   });
   socket.on("disconnect", reason => {
     console.log(reason);
     userNum--;
+    io.emit("get new message", {
+      type: "noti",
+      name: socket.username,
+      message: "님이 모닥불을 떠났습니다."
+    });
     io.emit("get user number", { userNum });
   });
 });
